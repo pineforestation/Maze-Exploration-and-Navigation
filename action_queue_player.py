d459e87167ae4e0d10d182949602811b0e4c738a
Douglas de Jesus <douglasdejesus92@gmail.com>
2023-10-24 23:12:13 -0400

    Initial commit

---
+++ action_queue_player.py
@@ -0,0 +1,100 @@
+from vis_nav_game import Player, Action
+import pygame
+import cv2
+from time import sleep
+
+class ActionQueuePlayer(Player):
+    """
+    Executes a series of predefined actions that are listed in self.actions
+    """
+
+    def __init__(self):
+        self.fpv = None
+        self.screen = None
+        self.actions = [Action.IDLE] * 100 + [Action.LEFT] * 147 + [Action.IDLE] * 100 + [Action.LEFT] * 147 + [Action.QUIT]
+        super(ActionQueuePlayer, self).__init__()
+
+    def reset(self):
+        self.fpv = None
+        self.screen = None
+        self.actions = [Action.IDLE] * 100 + [Action.LEFT] * 147 + [Action.IDLE] * 100 + [Action.LEFT] * 147 + [Action.QUIT]
+
+        pygame.init()
+
+    def act(self):
+        action = Action.IDLE
+        state = self.get_state()
+        if state is not None:
+          step = state[2]
+          if step < len(self.actions):
+            action = self.actions[step]
+
+        sleep(0.01)
+    
+        return action
+
+    def show_target_images(self):
+        targets = self.get_target_images()
+        if targets is None or len(targets) <= 0:
+            return
+        hor1 = cv2.hconcat(targets[:2])
+        hor2 = cv2.hconcat(targets[2:])
+        concat_img = cv2.vconcat([hor1, hor2])
+
+        w, h = concat_img.shape[:2]
+        
+        color = (0, 0, 0)
+
+        concat_img = cv2.line(concat_img, (int(h/2), 0), (int(h/2), w), color, 2)
+        concat_img = cv2.line(concat_img, (0, int(w/2)), (h, int(w/2)), color, 2)
+
+        w_offset = 25
+        h_offset = 10
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        line = cv2.LINE_AA
+        size = 0.75
+        stroke = 1
+
+        cv2.putText(concat_img, 'Front View', (h_offset, w_offset), font, size, color, stroke, line)
+        cv2.putText(concat_img, 'Right View', (int(h/2) + h_offset, w_offset), font, size, color, stroke, line)
+        cv2.putText(concat_img, 'Back View', (h_offset, int(w/2) + w_offset), font, size, color, stroke, line)
+        cv2.putText(concat_img, 'Left View', (int(h/2) + h_offset, int(w/2) + w_offset), font, size, color, stroke, line)
+
+        cv2.imshow(f'KeyboardPlayer:target_images', concat_img)
+        cv2.waitKey(1)
+
+    def set_target_images(self, images):
+        super(ActionQueuePlayer, self).set_target_images(images)
+        self.show_target_images()
+
+    def see(self, fpv):
+        if fpv is None or len(fpv.shape) < 3:
+            return
+
+        self.fpv = fpv
+
+        if self.screen is None:
+            h, w, _ = fpv.shape
+            self.screen = pygame.display.set_mode((w, h))
+
+        def convert_opencv_img_to_pygame(opencv_image):
+            """
+            Convert OpenCV images for Pygame.
+
+            see https://blanktar.jp/blog/2016/01/pygame-draw-opencv-image.html
+            """
+            opencv_image = opencv_image[:, :, ::-1]  # BGR->RGB
+            shape = opencv_image.shape[1::-1]  # (height,width,Number of colors) -> (width, height)
+            pygame_image = pygame.image.frombuffer(opencv_image.tobytes(), shape, 'RGB')
+
+            return pygame_image
+
+        pygame.display.set_caption("KeyboardPlayer:fpv")
+        rgb = convert_opencv_img_to_pygame(fpv)
+        self.screen.blit(rgb, (0, 0))
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    import vis_nav_game
+    vis_nav_game.play(the_player=ActionQueuePlayer())
